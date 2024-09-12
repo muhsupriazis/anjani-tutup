f080e03ab47c2bda114a344ff4d610ad01c3af5e
shixbt <nanashieth@gmail.com>
2024-09-12 13:52:12 +0800

    fic

--- app/(admin)/dashboard/components/data-table-toolbar.tsx
+++ app/(admin)/dashboard/components/data-table-toolbar.tsx
@@ -26,7 +26,7 @@ export function DataTableToolbar<TData>({
     <div className="flex items-center justify-between">
       <div className="flex flex-1 items-center space-x-2">
         <Input
-          placeholder="Masukan usernmame!"
+          placeholder="Masukan username!"
           value={(table.getColumn("username")?.getFilterValue() as string) ?? ""}
           onChange={(event) =>
             table.getColumn("username")?.setFilterValue(event.target.value)
